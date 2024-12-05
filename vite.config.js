6fa3a2e06c052f3f8a4cc099c834d8f7f1e60f20
danielmosvich <mrdaniraymay@gmail.com>
2024-12-04 23:50:47 -0500

    some

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,9 @@ export default defineConfig({
         female: 'female/index.html',
         kids: 'kids/index.html',
         login: 'login/index.html',
+        register: 'register/index.html',
         carrito: 'carrito/index.html',
-        
+
         // Agrega más páginas según tu estructura
       }
     }
