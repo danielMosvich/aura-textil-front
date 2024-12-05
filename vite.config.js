a2bc8883b97b2c1c38ba7083179a9117726fbdd0
danielmosvich <mrdaniraymay@gmail.com>
2024-12-04 23:48:06 -0500

    solved routes

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
       input: {
         main: 'index.html',
         male: 'male/index.html',
+        female: 'female/index.html',
+        kids: 'kids/index.html',
+        login: 'login/index.html',
+        carrito: 'carrito/index.html',
+        
         // Agrega más páginas según tu estructura
       }
     }
